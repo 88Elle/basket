8977eac6a0aeb12c2e8488ecdb9f2555a60ff7ab
elle <elle88@bk.ru>
2022-01-11 22:37:12 +0300

    save

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let cart = receipt();
+let menu = 'Вы заказали ';
+let delivery = 9000;
+
+for(let key in cart){
+   menu = menu + `${key}`;
+   for(let newKey in cart[key]){
+       menu = newKey == 'info' ? menu + `${cart[key][newKey]}` : menu;
+       delivery = newKey == 'price' ? delivery + cart[key][newKey] : delivery;
+
+   }
+}
+console.log(`${menu} | Общая стоимость ${delivery} сумм с доставкой (9000) `);
